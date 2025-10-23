0a5e02c1fb6255e202a75aa55ccca6f1486ad03d
BungaDoR <oktaviab14@gmail.com>
2025-10-23 21:56:37 +0700

    cobaBikinListView

--- lib/uts/menu_activity.dart
+++ lib/uts/menu_activity.dart
@@ -5,8 +5,68 @@ class MenuActivity extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Map<String,dynamic>> Items=[
+    {
+      "kode_produk":"PRD-001",
+      "deskripsi":"Good Day Freeze",
+      "harga":15000,
+      "stok":5,
+      "rating":4.8,
+      "gambar":"gambar/001.jpg"
+    },
+    {
+      "kode_produk":"PRD-002",
+      "deskripsi":"Kapal Api",
+      "harga":20000,
+      "stok":6,
+      "rating":4.7,
+      "gambar":"gambar/002.jpg"
+    },
+    {
+      "kode_produk":"PRD-003",
+      "deskripsi":"Torabika Coffee",
+      "harga":21000,
+      "stok":5,
+      "rating":4.8,
+      "gambar":"gambar/003.jpg"
+    },
+    {
+      "kode_produk":"PRD-004",
+      "deskripsi":"Pikopi ",
+      "harga":13000,
+      "stok":5,
+      "rating":4.4,
+      "gambar":"gambar/004.jpg"
+    },
+  ];
+
+
     return Scaffold(
-      body: Stack(),
+       body: ListView.builder(
+        itemCount: Items.length,
+        itemBuilder: (context, index){
+          Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 50,),
+            child: Container(
+              height: 50,
+              width: 90,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(topRight: Radius.circular(8),
+                bottomRight: Radius.circular(8)),
+                color: Colors.grey,
+                image: DecorationImage(image: AssetImage("gambar/Logo.png")),
+              ),
+              child: Text("LKS",style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 30, color: const Color.fromARGB(255, 53, 88, 105)
+                  ),),
+                  
+            ),
+          )
+        ],
+      );
+        }) 
     );
   }
 }
