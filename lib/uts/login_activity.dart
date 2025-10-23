fc12843582c3f366e8f45743f28a6b5359ab7db4
BungaDoR <oktaviab14@gmail.com>
2025-10-23 19:09:01 +0700

    lksMart

---
+++ lib/uts/login_activity.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoginActivity extends StatelessWidget {
+  const LoginActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("gambar/Logo.png"),
+              Column(
+                children: [
+                  Text("LKS",style: TextStyle(
+                    fontWeight: 
+                  ),),
+                  Text("MART")
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
